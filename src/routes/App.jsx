c3b6c021b42ecdcbc5848f97d38a44da19440daf
Martin <martinroldan889@gmail.com>
2021-08-10 11:34:45 -0300

    Update routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/jest-react" component={Home} />
         <Route exact path="/checkout" component={Checkout} />
         <Route component={NotFound} />
       </Switch>
